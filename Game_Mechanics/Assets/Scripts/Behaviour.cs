accbb9cf728cd509d253b401e4624dc7103dcec2
Scott Schepers <schepers.scott@hotmail.com>
2017-10-05 09:51:25 +0200

    adds to scene and behaviour script

---
+++ Game_Mechanics/Assets/Scripts/Behaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviour : MonoBehaviour {
+
+    public string state;
+    public bool infection = false;
+	// Use this for initialization
+	void Start () {
+        state = "human";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(infection == true) state = "zombie";
+        switch (state)
+        {
+            case "human":
+                HumanBehabiour();
+                break;
+
+            case "zombie":
+                ZombieBehaviour();
+                break;
+
+            default: break;
+        }
+		if(state == "human")
+        {
+
+        }
+	}
+
+    void HumanBehabiour()
+    {
+
+    }
+
+    void ZombieBehaviour()
+    {
+
+    }
+}
