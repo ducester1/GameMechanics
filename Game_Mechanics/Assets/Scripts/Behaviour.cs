8146865316435efe11c20d7ee21b28fa757c94ed
Scott Schepers <schepers.scott@hotmail.com>
2017-10-24 20:42:25 +0200

    changes to behaviour

--- Game_Mechanics/Assets/Scripts/Behaviour.cs
+++ Game_Mechanics/Assets/Scripts/Behaviour.cs
@@ -2,45 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Behaviour : MonoBehaviour {
-
-    public string state;
-    public bool infection = false;
-	// Use this for initialization
-	void Start () {
-        state = "human";
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        if(infection == true) state = "zombie";
-        switch (state)
-        {
-            case "human":
-                HumanBehabiour();
-                break;
-
-            case "zombie":
-                ZombieBehaviour();
-                break;
-
-            default: break;
-        }
-		if(state == "human")
-        {
-
-        }
-	}
-
-    void HumanBehabiour()
+namespace Behaviour
+{
+    public class Behaviour : MonoBehaviour
     {
 
+        public State currentState;
+        public Material normal, religious;
+        public Renderer rend;
+
+        // Use this for initialization
+        void Start()
+        {
+            currentState = new Normal();
+            rend = GetComponent<Renderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            currentState.Execute(this);
+        }
+
+        public void ChangeState(State newState)
+        {
+            currentState.Exit(this);
+            currentState = newState;
+            currentState.Enter(this);
+        }
     }
 
-    void ZombieBehaviour()
-    {
+        public abstract class State
+        {
+            public abstract void Enter(Behaviour human);
 
+            public abstract void Execute(Behaviour human);
+
+            public abstract void Exit(Behaviour human);
+        }
+
+        ////////////////////////////////////////////////States///////////////////////////////////////////
+
+    class Normal : State
+    {
+        public override void Enter(Behaviour human)
+        {
+            human.rend.material = human.normal;
+        }
+
+        public override void Execute(Behaviour human)
+        {
+            if (Input.anyKey) human.ChangeState(new Religious());
+        }
+
+        public override void Exit(Behaviour human)
+        {
+            
+        }
+    }
+
+    class Religious : State
+    {
+        public override void Enter(Behaviour human)
+        {
+            human.rend.material = human.religious;
+        }
+
+        public override void Execute(Behaviour human)
+        {
+            if (Input.anyKey) human.ChangeState(new Normal());
+        }
+
+        public override void Exit(Behaviour human)
+        {
+            
+        }
     }
 }
