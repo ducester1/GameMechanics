6fa95ff0ad77e681cccfc1ee306cd2ceecf6d879
Scott <schepers.scott@hotmail.com>
2018-04-11 22:02:50 +0200

    -Added new camera movement script

---
+++ Game_Mechanics/Assets/CameraMovement2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement2 : MonoBehaviour
+{
+    private Vector3 mouseOrigin;
+    private bool isPanning;
+    private Vector3 currentPosition;
+    public float panSpeed = 10;
+    private Vector3 initialPosition;
+    private float yRotation;
+    private float xRotation;
+    public float rotationSpeed = 10;
+    private Vector2 initialRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = transform.position;
+        float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseOrigin = Input.mousePosition;
+            isPanning = true;
+        }
+        // cancel on button release
+        if (!Input.GetMouseButton(0))
+        {
+            isPanning = false;
+        }
+        //move camera on X & Y
+        else if (isPanning)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+            Vector3 move = new Vector3(pos.x * -panSpeed, pos.y * -panSpeed, 0);
+            Camera.main.transform.Translate(move, Space.Self);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+            transform.localPosition = new Vector3(transform.position.x, transform.position.y - (transform.position.y - (initialPosition.y)), transform.position.z);
+        }
+        //rotate camera with mousewheel click
+        if (Input.GetMouseButton(2))
+        {
+            yRotation -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+            yRotation = Mathf.Clamp(yRotation, -80, 80);
+            xRotation += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            xRotation = xRotation % 360;
+            transform.localEulerAngles = new Vector3(yRotation + initialRotation.x, xRotation + initialRotation.y, 0);
+        }
+        //zoom with scroll
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 10000f * scrollAxis, Space.Self);
+            initialPosition = transform.position;
+        }
+    }
+}
--- Game_Mechanics/Assets/Scripts/CameraMovement.cs
+++ Game_Mechanics/Assets/Scripts/CameraMovement.cs
@@ -2,36 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
+{
 
     Rigidbody rb;
-    public float speed = 10;
-    public float ZoomAmount = 0;
-    public float MaxToClamp = 10;
-    public float ROTSpeed = 10;
+    public float maxSpeed;
+    public float minSpeed;
+
+    public float speed;
+
+    public float ZoomAmount = 0f;
+    public float MaxToClamp = 10f;
+    public float zoomSpeed = 30f;
+
+    public float minY = 10f;
+    public float maxY = 80f;
+
+    private float zoomingArea;
+    private float minMaxSpeedDif;
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
+        zoomingArea = maxY - minY;
+        minMaxSpeedDif = maxSpeed - minSpeed;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //heen en weer bewegen
-        speed = ZoomAmount * 16;
-        if (speed >= 0) speed = -1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moving around
+        speed = (transform.position.y - minY) / zoomingArea;
+        speed = minMaxSpeedDif * speed;
+
+        //if (speed >= -16) speed = -16;
         float vx = Input.GetAxis("Horizontal");
         float vz = Input.GetAxis("Vertical");
-        rb.velocity = new Vector3(vx, 0, vz) * -speed;
+        Debug.Log("vx = " + vx + " vz = " + vz);
+        rb.velocity = new Vector3(vx, 0, vz) * Mathf.Abs(speed);
 
+        //zooming
         ZoomAmount += Input.GetAxis("Mouse ScrollWheel");
         ZoomAmount = Mathf.Clamp(ZoomAmount, -MaxToClamp, MaxToClamp);
-        if (ZoomAmount < 0)
+        if (transform.position.y >= minY && transform.position.y <= maxY)
         {
             var translate = Mathf.Min(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")), MaxToClamp - Mathf.Abs(ZoomAmount));
-            gameObject.transform.Translate(0, 0, translate * ROTSpeed * Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")));
+            gameObject.transform.Translate(0, 0, translate * zoomSpeed * Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")));
         }
-        else ZoomAmount = 0;
+        else if (transform.position.y < minY) transform.position = new Vector3(transform.position.x, minY, transform.position.z);
+        else if (transform.position.y > maxY) transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
     }
 }
