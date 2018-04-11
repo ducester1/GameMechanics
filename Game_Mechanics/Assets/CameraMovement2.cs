26211c930f1a7d35b6e542abad19b65ff17d91a6
Scott <schepers.scott@hotmail.com>
2018-04-11 23:24:48 +0200

    -Added movement by wasd and panning at the side of the screen

--- Game_Mechanics/Assets/CameraMovement2.cs
+++ Game_Mechanics/Assets/CameraMovement2.cs
@@ -7,16 +7,19 @@ public class CameraMovement2 : MonoBehaviour
     public float scrollSpeed;
     public float rotationSpeed;
     public float panSpeed;
+    public float wasdSpeed;
     public float minZoom;
     public float maxZoom;
+    public float panBorderThickness;
 
-    private Vector3 mouseOrigin;
-    private Vector3 currentPosition;
     private Vector3 initialPosition;
     private Vector3 initialRotation;
+    private Vector3 initialMousePosition;
+    private Vector3 wasdPos;
     private bool isPanning;
     private float yRotation;
     private float xRotation;
+
 
 
 
@@ -28,26 +31,45 @@ public class CameraMovement2 : MonoBehaviour
         initialRotation = transform.localEulerAngles;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(initialRotation);
-        currentPosition = transform.position;
-        float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
         if (Input.GetMouseButtonDown(0))
         {
-            mouseOrigin = Input.mousePosition;
             isPanning = true;
         }
-        // cancel on button release
+        //move camera on X & Y with WASD or bij panning at the side of the screen
+        wasdPos = transform.position;
+        if ((Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) && !isPanning)
+        {
+            wasdPos += Vector3.forward * wasdSpeed * Time.deltaTime;
+        }
+        if ((Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) && !isPanning)
+        {
+            wasdPos += Vector3.back * wasdSpeed * Time.deltaTime;
+        }
+        if ((Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) && !isPanning)
+        {
+            wasdPos += Vector3.left * wasdSpeed * Time.deltaTime;
+        }
+        if ((Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) && !isPanning)
+        {
+            wasdPos += Vector3.right * wasdSpeed * Time.deltaTime;
+        }
+        if (wasdPos != transform.position)
+        {
+            transform.position = wasdPos;
+        }
+
+        //cancel on button release
         if (!Input.GetMouseButton(0))
         {
             isPanning = false;
         }
-        //move camera on X & Y
-        else if (isPanning)
+
+        //move camera on X & Y with mouse
+        else if (isPanning && Input.mousePosition != initialMousePosition)
         {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - initialMousePosition);
             Vector3 move = new Vector3(pos.x * -panSpeed, pos.y * -panSpeed, 0);
             Camera.main.transform.Translate(move, Space.Self);
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
@@ -56,8 +78,9 @@ public class CameraMovement2 : MonoBehaviour
         //rotate camera with mousewheel click
         if (Input.GetMouseButton(2))
         {
+            Debug.Log(yRotation);
             yRotation -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
-            yRotation = Mathf.Clamp(yRotation, -80, 80);
+            yRotation = Mathf.Clamp(yRotation, -60, 30);
             xRotation += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
             xRotation = xRotation % 360;
             transform.localEulerAngles = new Vector3(yRotation + initialRotation.x, xRotation + initialRotation.y, 0);
@@ -66,9 +89,11 @@ public class CameraMovement2 : MonoBehaviour
         if (Input.GetAxis("Mouse ScrollWheel") != 0 && transform.position.y >= minZoom && transform.position.y <= maxZoom)
         {
             initialPosition = transform.position;
-            transform.Translate(Vector3.forward * Time.deltaTime * scrollSpeed * scrollAxis, Space.Self);
+            transform.Translate(Vector3.forward * Time.deltaTime * scrollSpeed * Input.GetAxis("Mouse ScrollWheel"), Space.Self);
             if (transform.position.y < minZoom || transform.position.y > maxZoom) transform.position = initialPosition;
             initialPosition = transform.position;
         }
+
+        initialMousePosition = Input.mousePosition;
     }
 }
